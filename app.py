3a49231894e8e6ef8c177fdaf13c28090c807385
eslam sharoud <125641180+Eslam2033@users.noreply.github.com>
2025-11-18 22:00:17 +0200

    update

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st
+from modules.RAG import get_pdf_text
+from modules.RAG import get_chunk_text
 
-st.header("Welcome to the Streamlit App")
+st.header("Chat with multiple pdf")
+uploaded_files = st.file_uploader("Upload PDF files", accept_multiple_files=True, type=["pdf"])
